c8a1721dd1e0444c3f4ec2d3f7c663adb2b30654
xdite <xuite.joke@gmail.com>
2016-09-13 18:28:55 +0800

    plan crud

--- app/controllers/admin/plans_controller.rb
+++ app/controllers/admin/plans_controller.rb
@@ -18,6 +18,29 @@ class Admin::PlansController < AdminController
     end
   end
 
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  def update
+    @plan = Plan.find(params[:id])
+
+    if @plan.update(plan_params)
+      redirect_to admin_plans_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @plan = Plan.find(params[:id])
+
+    @plan.destroy
+    redirect_to admin_plans_path
+  end
+
+  protected
+
   def plan_params
     params.require(:plan).permit(:name, :plan_date, :price)
   end
