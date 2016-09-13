34d800870bd94f93a5ff90acd36f57be7a71d7f3
shaojunda <shaojunda@gmail.com>
2016-09-13 19:40:16 +0800

    update seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913095853) do
+ActiveRecord::Schema.define(version: 20160913113504) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
