12cd267a94e4ad4bafe90b7a351b8a33bb3d92e7
liber17321 <tianwh3721@gmail.com>
2016-09-13 19:30:55 +0800

    admin can edit & delete book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,2 +1,55 @@
-class Admin::BooksController < AdminController
+class Admin::BooksController < ApplicationController
+  before_action :find_book_by_id, only: [:edit, :update, :destroy, :publish, :offline]
+  layout "admin"
+  
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+      if @book.save
+        flash[:notice] = "浓缩书创建成功"
+        redirect_to admin_books_path
+      else
+        render :new
+      end
+
+  end
+
+  def update
+    if @book.update(book_params)
+      flash[:notice] = "浓缩书更新成功"
+      redirect_to admin_books_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    flash[:alert] = "浓缩书删除成功"
+    redirect_to admin_books_path
+  end
+
+
+  protected
+
+  def find_book_by_id
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :content)
+  end
+
 end
