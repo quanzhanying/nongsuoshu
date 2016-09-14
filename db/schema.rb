188f52719703dd324718f06451660ba2b2f8db84
liber17321 <tianwh3721@gmail.com>
2016-09-14 13:25:00 +0800

    change user status after paying

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160914030615) do
-
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -44,12 +42,13 @@ ActiveRecord::Schema.define(version: 20160914030615) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "favorites_relationships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
   create_table "notifications", force: :cascade do |t|
     t.integer  "recipient_id"
     t.integer  "actor_id"
@@ -59,7 +58,6 @@ ActiveRecord::Schema.define(version: 20160914030615) do
     t.string   "notifiable_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-
   end
 
   create_table "orders", force: :cascade do |t|
