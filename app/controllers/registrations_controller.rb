12dc75351af014d2a933f0e7f1a4601cf001fabe
liber17321 <tianwh3721@gmail.com>
2016-09-14 20:48:04 +0800

    fix footer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
+    # binding.pry
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
