456a9b3bc9056a2a7f19592f8cc6f26dea8f1609
yanzeng li <yanzengli@gmail.com>
2016-09-14 11:22:20 +0800

    liyanzeng-modify-user-change-password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "devise/registrations", :change_password => "registrations"}
 
   resources :categories
 
